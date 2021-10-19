df305cb7e5d6edfa27cc61375ea182cec0af4908
Laurent Lucas <59290798+laurent-lu@users.noreply.github.com>
2021-10-19 15:20:54 +0200

    Scripts update

--- scripts/p2020.js
+++ scripts/p2020.js
@@ -3,11 +3,11 @@
 
 document.write('<tr><td><img class="publication_img" src="img/teaser/siggraph2020_teaser.png"><p></p></td>')
 document.write('  <td class="publication_data">')
-document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> Transfer matrix based layered materials rendering</div>')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> A six-flux transfer approach for efficient layered materials rendering</div>')
 document.write('    <div class="authors">J. Randrianandrasana, P. Callet and L. Lucas</div>')
 document.write('    <div class="venue">ACM Siggraph 2020 Posters, Washington, USA, August 2020. DOI:10.1145/3388770.3407453</div>')
 document.write('    <div class="links">')
-document.write('      <a href="https://hal.univ-reims.fr/hal-01982960/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf</a>')
+document.write('      <a href="https://hal.univ-reims.fr/hal-02977384/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf</a>')
 document.write('      &nbsp;')
 document.write('    </div>')
 document.write('  </td>')
