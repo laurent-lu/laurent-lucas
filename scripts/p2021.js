badcb76990e33276beda0caac51dc3619b1f7c34
Laurent Lucas <59290798+laurent-lu@users.noreply.github.com>
2021-10-21 14:16:37 +0200

    Script update

--- scripts/p2021.js
+++ scripts/p2021.js
@@ -3,7 +3,7 @@
 
 document.write('<tr><td><img class="publication_img" src="img/teaser/siggraph2021_teaser.png"><p></p></td>')
 document.write('  <td class="publication_data">')
-document.write('    <div class="title"><i class="fa fa-file-text" aria-hidden="true"></i> Transfer matrix based layered materials rendering</div>')
+document.write('    <div class="title"><i class="fa fa-file-text" aria-hidden="true"></i> <i class="fa fa-users" aria-hidden="true"></i> Transfer matrix based layered materials rendering</div>')
 document.write('    <div class="authors">J. Randrianandrasana, P. Callet and L. Lucas</div>')
 document.write('    <div class="venue">ACM Trans. on Graph. (Proceedings of SIGGRAPH), 40(4), August 2021. DOI:10.1145/3450626.3459859</div>')
 document.write('    <div class="links">')
