73dff5e35d3b5730563ddc15b725104e792b6c81
Laurent Lucas <59290798+laurent-lu@users.noreply.github.com>
2023-06-06 10:25:45 +0200

    Add files via upload
    Add script files p22 and 23

---
+++ scripts/p2022.js
@@ -0,0 +1,32 @@
+<!-- document.write('')-->
+<!-- generated on May 4, 2021 at 12:30 pm -->
+
+document.write('<tr><td><img class="publication_img" src="img/teaser/eg2022-poster1_teaser.png"><p></p></td>')
+document.write('  <td class="publication_data">')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> Time series AMR data representation for out-of-core interactive visualization</div>')
+document.write('    <div class="authors">W. Alexandre-Barff, H. Deleau, J. Sarton, F. Ledoux and L. Lucas</div>')
+document.write('    <div class="venue">43nd Annual Conference of Eurographics (EG), Reims, France, April 2022. DOI:10.2312/egp.20221014</div>')
+document.write('    <div class="links">')
+document.write('      <a href="https://hal.univ-reims.fr/hal-01982960/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf</a>')
+document.write('      &nbsp;')
+document.write('      <a href="https://hal.univ-reims.fr/hal-01982960/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> poster</a>')
+document.write('      &nbsp;')
+document.write('    </div>')
+document.write('  </td>')
+document.write('</tr>')
+
+document.write('<tr><td><img class="publication_img" src="img/teaser/eg2022-poster2_teaser.png"><p></p></td>')
+document.write('  <td class="publication_data">')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> Neural Denoising for Spectral Monte Carlo Rendering</div>')
+document.write('    <div class="authors">R. Rouphael, M. Noizet, S. Prévost, H. Deleau, L-A. Steffenel and L. Lucas</div>')
+document.write('    <div class="venue">43nd Annual Conference of Eurographics (EG), Reims, France, April 2022. DOI:10.2312/egp.20221011</div>')
+document.write('    <div class="links">')
+document.write('      <a href="https://hal.univ-reims.fr/hal-04042962/file/EG22.pdf" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf</a>')
+document.write('      &nbsp;')
+document.write('      <a href="https://hal.univ-reims.fr/hal-04042962/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> poster</a>')
+document.write('      &nbsp;')
+document.write('    </div>')
+document.write('  </td>')
+document.write('</tr>')
+
+<!-- document.write('')-->
---
+++ scripts/p2023.js
@@ -0,0 +1,28 @@
+<!-- document.write('')-->
+<!-- generated on May 4, 2021 at 12:30 pm -->
+
+document.write('<tr><td><img class="publication_img" src="img/teaser/egpgv2023_teaser.png"><p></p></td>')
+document.write('  <td class="publication_data">')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> A GPU-based out-of-core architecture for interactive visualization of AMR time series data</div>')
+document.write('    <div class="authors">W. Alexandre-Barff, H. Deleau, J. Sarton, F. Ledoux and L. Lucas</div>')
+document.write('    <div class="venue">Eurographics Symposium on Parallel Graphics and Visualization (EGPGV), Leipzig, Germany, June 2023. DOI:</div>')
+document.write('    <div class="links">')
+document.write('      <a href="https://hal.univ-reims.fr/hal-01982960/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf</a>')
+document.write('      &nbsp;')
+document.write('    </div>')
+document.write('  </td>')
+document.write('</tr>')
+
+document.write('<tr><td><img class="publication_img" src="img/teaser/eurovis2023_teaser.png"><p></p></td>')
+document.write('  <td class="publication_data">')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> State-of-the-art in Large-Scale Volume Visualization, Beyond Structured Data</div>')
+document.write('    <div class="authors">J. Sarton, S. Zellmann, S. Demirci, U. Güdükbay, W. Alexandre-Barff, L. Lucas, J.M. Dischler, S. Wesner and I. Wald</div>')
+document.write('    <div class="venue">25th Eurographics Conference on Visualization (EuroVis), Leipzig, Germany, June 2023. DOI:</div>')
+document.write('    <div class="links">')
+document.write('      <a href="https://hal.univ-reims.fr/hal-01982960/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf</a>')
+document.write('      &nbsp;')
+document.write('    </div>')
+document.write('  </td>')
+document.write('</tr>')
+
+<!-- document.write('')-->
