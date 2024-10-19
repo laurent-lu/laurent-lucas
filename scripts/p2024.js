f9694326cc631938c9f3cc149cf087cd9fdf39db
Laurent Lucas <59290798+laurent-lu@users.noreply.github.com>
2024-10-19 18:20:14 +0200

    Add and update 2023 and 2024 publications

---
+++ scripts/p2024.js
@@ -0,0 +1,28 @@
+<!-- document.write('')-->
+<!-- generated on Oct 19, 2024 at 5:30 pm -->
+
+document.write('<tr><td><img class="publication_img" src="img/teaser/no-image-available.png"><p></p></td>')
+document.write('  <td class="publication_data" style="color:gray;">')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> <em>Spectral Monte Carlo Denoiser</em></div>')
+document.write('    <div class="authors">M. Noizet, R. Rouphael, S. Prévost, H. Deleau, L-A. Steffenel and L. Lucas</div>')
+document.write('    <div class="venue">Submitted to Computer & Graphics</div>')
+document.write('    <div class="links">')
+document.write('      <i class="fa fa-file-pdf-o" aria-hidden="true"></i> pdf')
+document.write('      &nbsp;')
+document.write('    </div>')
+document.write('  </td>')
+document.write('</tr>')
+
+document.write('<tr><td><img class="publication_img" src="img/teaser/no-image-available.png"><p></p></td>')
+document.write('  <td class="publication_data">')
+document.write('    <div class="title"><i class="fa fa-users" aria-hidden="true"></i> Spectral Monte Carlo Denoiser</div>')
+document.write('    <div class="authors">M. Noizet, R. Rouphael, S. Prévost, H. Deleau, L-A. Steffenel and L. Lucas</div>')
+document.write('    <div class="venue">High-Performance Graphics (HPG), Denver, USA, Jul 2024.</div>')
+document.write('    <div class="links">')
+document.write('      <a href="https://hal.univ-reims.fr/hal-04716800/document" target="_blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> joint poster HPG and SIGGRAPH</a>')
+document.write('      &nbsp;')
+document.write('    </div>')
+document.write('  </td>')
+document.write('</tr>')
+
+<!-- document.write('')-->
